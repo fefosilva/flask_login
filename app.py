279079121635736745c0d710583fe979246c2c66
fefo <silva.martinsf@gmail.com>
2024-11-04 22:55:59 -0300

    feat: add landing page route and home.html file

--- app.py
+++ app.py
@@ -36,3 +36,11 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+@app.route('/')
+def home():
+    """
+    Landing page
+    
+    """
+    return render_template('home.html')
+
